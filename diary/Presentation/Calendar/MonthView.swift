b569d3498139e090ba14608f149fb956bed118cf
Fernando Salom <fer@rudo.es>
2024-06-25 16:49:44 +0200

    Fix month view

--- diary/Presentation/Calendar/MonthView.swift
+++ diary/Presentation/Calendar/MonthView.swift
@@ -18,16 +18,21 @@ struct MonthView: View {
     private let columns = Array(repeating: GridItem(.flexible()), count: 7)
 
     var body: some View {
-            VStack {
+        VStack(spacing: 0) {
                 Text(viewModel.monthName(from: month))
                     .font(.footnote)
-                HStack {
+            /*
+                HStack(spacing: 0) {
                     ForEach(viewModel.weekDaysSingle, id: \.self) { day in
                         Text(day)
                             .font(.footnote)
                             .frame(maxWidth: .infinity)
+                            .padding(0)
                     }
                 }
+                .background(Color.blue)
+                .frame(maxWidth: .infinity, alignment: .leading)
+             */
                 LazyVGrid(columns: columns, spacing: 0) {
                     ForEach(days) { day in
                         DayView(day: day)
